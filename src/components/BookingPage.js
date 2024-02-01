0b824c77804c5e4c71cf77f587273dbf28d2cc10
Beliya470 <anneanziay78@gmail.com>
2024-02-02 01:17:46 +0300

    Your commit message

---
+++ src/components/BookingPage.js
@@ -0,0 +1,304 @@
+
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker'; 
+import './BookingPage.css'; 
+import 'react-datepicker/dist/react-datepicker.css';
+import whatsappIcon from './whatsapp.png'; // Adjust the path if necessary
+
+
+
+
+
+
+
+
+
+function BookingPage() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [bookingRoomId, setBookingRoomId] = useState(null);
+
+  // Remove this line:
+  const [bookingSuccess, setBookingSuccess] = useState(false);
+
+  const navigate = useNavigate();
+
+
+  const API_URL = 'https://easydine2024-8.onrender.com'; // Backend API URL
+  const [showBookingForm, setShowBookingForm] = useState(false);
+ 
+
+  // const [bookingSuccess, setBookingSuccess] = useState(false);
+
+  // const handleBookingSubmission = (event) => {
+  //   event.preventDefault();
+  //   // Implement your booking logic here.
+  //   // After the booking logic:
+  //   setBookingSuccess(true); // Set the success message to true
+  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call the function to fetch rooms or handle the submission logic here
+    handleFetchRooms(); // This will fetch rooms when the form is submitted
+  };
+  
+  
+
+
+
+  const handleBookRoom = (roomId) => {
+    
+    setBookingRoomId(roomId); // Set the current room id for booking
+    // You might want to toggle visibility of the date-picker here or navigate the user to the booking details page
+  };
+
+  const confirmDates = () => {
+    setShowBookingForm(true);
+  };
+  const handleSuccessAcknowledgement = () => {
+    setBookingSuccess(false); // Hide the success message
+    setShowBookingForm(false);
+    setBookingRoomId(null);
+    // Here you can also reset form values or redirect the user as needed
+    // For example, to reset the form you could setStartDate(new Date()), setEndDate(new Date()), etc.
+    // To redirect the user, you could use window.location.href = '/some-path';
+  };
+
+
+    // Modify your SuccessMessage component
+const SuccessMessage = () => (
+  <div className="success-message">
+    <p>Reservation was successful!</p>
+    <button onClick={handleSuccessAcknowledgement} className="acknowledge-button">OK</button>
+  </div>
+);
+
+const handleBookingSubmission = async (event) => {
+  event.preventDefault();
+
+  const userId = sessionStorage.getItem('user_id'); // Retrieve user ID from storage
+
+  // Check if the user is logged in
+  if (!userId) {
+    alert('Please log in to confirm your booking.');
+    navigate('/login'); // Replace '/login' with your login route
+    return;
+  }
+
+  const bookingDetails = {
+    userId: userId,
+    roomId: bookingRoomId,
+    checkIn: startDate.toISOString(),
+    checkOut: endDate.toISOString(),
+  };
+
+  // Log the booking details to the console
+  console.log('Booking Details:', bookingDetails);
+
+  try {
+    const response = await fetch(`${API_URL}/make-booking`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${sessionStorage.getItem('jwt_token')}`,
+      },
+      body: JSON.stringify(bookingDetails)
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      setBookingSuccess(true);
+    } else {
+      console.error('Booking error:', data);
+    }
+  } catch (error) {
+    console.error('Error submitting booking:', error);
+  }
+};
+
+
+
+
+
+
+
+  const handleDateChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+    // Here you would handle the date change, perhaps updating state or making a booking API call
+  };
+
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    checkIn: '',
+    checkOut: '',
+    roomType: '',
+    guests: 1,
+  });
+
+ 
+  
+    const [availableRooms, setAvailableRooms] = useState([]);
+  
+    // useEffect hook added to fetch rooms automatically on component mount
+    useEffect(() => {
+      handleFetchRooms();
+    }, []); // Empty array as second argument to only run once on mount
+  
+    const handleFetchRooms = () => {
+      const token = sessionStorage.getItem('jwt_token'); // Retrieve token from storage
+      fetch(`${API_URL}/booking`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => response.json())
+      .then(data => setAvailableRooms(data))
+      .catch(error => console.error('Error fetching available rooms:', error));
+    };
+
+  
+
+  const handleRoomSelection = (roomId) => {
+    console.log('Selected room ID:', roomId);
+    // Additional logic for room selection can be added here
+  };
+
+  const roomList = Array.isArray(availableRooms) ? availableRooms.map(room => (
+  //  const roomList = availableRooms.map(room => (
+    <div key={room.id} className="room-card">
+      {/* <img src={`${API_URL}/${room.image_url.replace('static/', '')}`} alt={room.category} className="room-image" /> */}
+      {/* <img src={`${API_URL}${room.image_url}`} alt={room.category} className="room-image" /> */}
+      <img src={`${API_URL}/${room.image_url.split('static/').pop()}`} alt={room.category} className="room-image" />
+
+      {/* <img src={`${API_URL}/static/${room.image_url}`} alt={room.category} className="room-image" /> */}
+      <div className="room-details">
+        <h2>{room.category}</h2>
+        <p>{room.style}</p>
+        {bookingRoomId === room.id ? (
+          <>
+            <div className="date-picker-container">
+              <DatePicker
+                selected={startDate}
+                onChange={handleDateChange}
+                startDate={startDate}
+                endDate={endDate}
+                selectsRange
+                inline
+              />
+              <button onClick={confirmDates} className="confirm-dates-button">Select Dates</button>
+            </div>
+            {showBookingForm && (
+              <form onSubmit={handleBookingSubmission} className="booking-form">
+                <input type="text" placeholder="Your Name" onChange={e => { /* your handler here */ }} required />
+                <input type="email" placeholder="Your Email" onChange={e => { /* your handler here */ }} required />
+                <button type="submit" className="submit-booking-button">Save Booking</button>
+              </form>
+            )}
+          </>
+        ) : (
+          <button onClick={() => handleBookRoom(room.id)} className="book-now-button">Book Now</button>
+        )}
+      </div>
+    </div>
+  )): null;
+  
+
+  return (
+    <div className="booking-page">
+      <section className="hero-section">
+        <div className="hero-content">
+          <h1>Discover Your Perfect Stay</h1>
+          <p>Explore our hotel's exquisite rooms and gourmet dining experiences for your next luxurious escape..</p>
+          <form onSubmit={handleSubmit} className="search-form">
+            {/* Input fields */}
+            {/* <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleInputChange} />
+            <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleInputChange} /> */}
+            {/* <button type="submit">Search</button> */}
+          </form>
+        </div>
+        <img src="https://images.pexels.com/photos/2417842/pexels-photo-2417842.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Hero" className="hero-image" />
+      </section>
+  
+      <section className="available-rooms" id="available-rooms-section">
+        <h2>Available Rooms</h2>
+        <div className="room-grid">
+
+        {Array.isArray(availableRooms) && availableRooms.map((room) => (
+          // {availableRooms.map((room) => (
+            <div key={room.id} className="room-card">
+              <img
+                src={`${API_URL}/${room.image_url}`}
+                alt={room.category}
+                className="room-image"
+              />
+              <div className="room-details">
+                <h2>{room.category}</h2>
+                <p>Style: {room.style}</p>
+                <p>Occupancy: {room.occupancy}</p>
+                <p>Size: {room.size}</p>
+                <p>Bed Type: {room.bed_type}</p>
+                <p>Price: £{room.price}</p>
+                
+                {bookingRoomId === room.id ? (
+                  <>
+                    <div className="date-picker-container">
+                      <DatePicker
+                        selected={startDate}
+                        onChange={handleDateChange}
+                        startDate={startDate}
+                        endDate={endDate}
+                        selectsRange
+                        inline
+                      />
+                      <button onClick={() => setBookingRoomId(null)} className="cancel-button">
+                        Cancel
+                      </button>
+                      {bookingSuccess ? (
+                        <SuccessMessage />
+                      ) : (
+                        <button onClick={handleBookingSubmission} className="confirm-dates-button">
+                          Confirm Booking
+                        </button>
+                      )}
+                    </div>
+                  </>
+                ) : (
+                  <button onClick={() => handleBookRoom(room.id)} className="book-now-button">
+                    Book Now
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      
+
+
+      <footer className="footer">
+  <div>
+    <h3>About Us</h3>
+    <p>At our hotel, we are committed to providing an unparalleled experience for our guests. Our team is dedicated to ensuring every stay is memorable, combining luxury with the comforts of home.</p>
+  </div>
+  <div>
+    <h3><a href="/contact" style={{ textDecoration: 'none', color: 'inherit' }}>Contact Us</a></h3>
+    
+    <p>If you have any inquiries or need assistance, please don't hesitate to <a href="/contact" style={{ textDecoration: 'none', color: 'inherit' }}>reach out</a>. <a href="https://wa.me/+447477181743" style={{ textDecoration: 'none', color: 'inherit' }}><img src={whatsappIcon} alt="WhatsApp" style={{ width: '114px', height: '20px' }}/></a></p>
+
+    {/* <p>If you have any inquiries or need assistance, please don't hesitate to <a href="/contact" style={{ textDecoration: 'none', color: 'inherit' }}>reach out</a>. <a href="https://wa.me/+447477181743" style={{ textDecoration: 'none', color: 'inherit' }}><img src="whatsapp.png" alt="WhatsApp"/></a></p> */}
+
+    {/* <p>If you have any inquiries or need assistance, please don't hesitate to <a href="/contact" style={{ textDecoration: 'none', color: 'inherit' }}>reach out</a>. <a href="https://wa.me/+447477181743" style={{ textDecoration: 'none', color: 'inherit' }}><img src="whatsapp_icon_url" alt="WhatsApp"/></a></p> */}
+  </div>
+</footer>
+
+    </div>
+  );
+}
+
+export default BookingPage;
---
+++ src/components/LoginPage.js
@@ -0,0 +1,189 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function AuthPage() {
+    const [isLogin, setIsLogin] = useState(true);
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+        confirmPassword: '',
+        is_admin: false,
+    });
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    
+
+    const API_URL = 'https://easydine2024-8.onrender.com';
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setError('');
+    
+        const endpoint = isLogin ? '/login' : '/register';
+        const payload = isLogin ? { 
+            username: credentials.username, 
+            password: credentials.password 
+        } : { 
+            username: credentials.username, 
+            password: credentials.password,
+            is_admin: credentials.is_admin
+        };
+    
+        if (!isLogin && credentials.password !== credentials.confirmPassword) {
+            setError("Passwords don't match.");
+            return;
+        }
+    
+        try {
+            const response = await axios.post(`${API_URL}${endpoint}`, payload, {
+                headers: { 'Content-Type': 'application/json' }
+            });
+        
+            console.log("Received from server:", response.data); // Debug: Log the entire response
+        
+            if (response.data.success) {
+                if (!isLogin) {
+                    alert('Registration successful. Please log in.');
+                    setIsLogin(true);
+                } else {
+                    // Correctly retrieve user_id from the response. Adjust this line based on your actual response structure.
+                    // If the user ID is directly in the response, use response.data.user_id or similar.
+                    const userId = response.data.user_id; // Adjust this according to your response structure
+            
+                    if (userId) {
+                        // sessionStorage.setItem('user_id', userId.toString()); // Ensure userId is a string
+                        // After successful login
+                        sessionStorage.setItem('user_id', response.data.user_id.toString());
+
+                        sessionStorage.setItem('jwt_token', response.data.token);
+                        sessionStorage.setItem('is_admin', response.data.is_admin.toString());
+                        
+                        navigate(response.data.is_admin ? '/admin' : '/booking');
+                    } else {
+                        console.error('User ID is undefined.');
+                        setError('Login failed due to server error.');
+                    }
+                }
+            } else {
+                setError(response.data.message || 'Invalid credentials.');
+            }
+            
+        } catch (error) {
+            console.error('Auth error:', error.response ? error.response.data : error);
+            setError('An unexpected error occurred');
+        }
+        
+    };
+    
+    
+    const handleChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    };
+
+    const pageStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#8ecae6',
+        transition: 'all 0.5s',
+        transform: isLogin ? 'translateY(0)' : 'translateY(-100px)',
+        opacity: isLogin ? 1 : 0.8,
+    };
+
+    const formStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '15px',
+        backgroundColor: '#fff',
+        padding: '30px',
+        borderRadius: '15px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        transition: 'all 0.5s',
+        transform: isLogin ? 'scale(1)' : 'scale(1.05)',
+    };
+
+    const buttonStyle = {
+        padding: '10px 15px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        backgroundColor: '#ffafcc',
+        color: 'white',
+        fontWeight: 'bold',
+    };
+
+    const switchButtonStyle = {
+        background: 'none',
+        border: 'none',
+        color: '#023047',
+        textDecoration: 'underline',
+        cursor: 'pointer',
+    };
+    
+
+    return (
+        <div style={pageStyle}>
+            <h1>{isLogin ? 'Welcome Back!' : 'Join Us!'}</h1>
+            {error && <div style={{ color: '#f94144' }}>{error}</div>}
+            <form onSubmit={handleSubmit} style={formStyle}>
+                <input
+                    name="username"
+                    type="text"
+                    value={credentials.username}
+                    onChange={handleChange}
+                    placeholder="Username"
+                    required
+                />
+                <input
+                    name="password"
+                    type="password"
+                    value={credentials.password}
+                    onChange={handleChange}
+                    placeholder="Password"
+                    required
+                />
+                {!isLogin && (
+                    <input
+                        name="confirmPassword"
+                        type="password"
+                        value={credentials.confirmPassword}
+                        onChange={handleChange}
+                        placeholder="Confirm Password"
+                        required
+                    />
+                )}
+                {!isLogin && (
+                    <label>
+                        <input
+                            name="is_admin"
+                            type="checkbox"
+                            checked={credentials.is_admin}
+                            onChange={(e) => setCredentials({ ...credentials, is_admin: e.target.checked })}
+                            // onChange={(e) => handleChange({ ...e, target: { ...e.target, value: e.target.checked ? 'true' : 'false' } })}
+                            // onChange={handleChange}
+                            // onChange={(e) => handleChange({ ...e, target: { ...e.target, name: 'is_admin', value: e.target.checked } })}
+                        />
+                        Register as admin
+                    </label>
+                )}
+                
+                <button type="submit" style={buttonStyle}>
+                    {isLogin ? 'Log In' : 'Register'}
+                </button>
+                <button
+                    type="button"
+                    onClick={() => setIsLogin(!isLogin)}
+                    style={switchButtonStyle}
+                >
+                    {isLogin ? 'New here? Register' : 'Already a member? Log In'}
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default AuthPage;
